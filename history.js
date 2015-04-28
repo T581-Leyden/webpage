6f43ccb3358a3cc32e626ec9c8023c28a073cba3
Ellen Brandenberger <ellencbrandenberger@gmail.com>
2015-04-28 15:29:35 -0400

    Update history.js

--- history.js
+++ history.js
@@ -1,3 +1,4 @@
+
 $("#rowone").show(function(){
     $("#rowtwo").hide();
     $("#rowthree").hide();
@@ -7,11 +8,55 @@ $(function() {
     $( "#wood" ).draggable();
     $( "#corn" ).draggable();  
     $( "#cotton" ).draggable();
-    $( "#bubble" ).draggable();  
-    $( "#shells" ).draggable();
+    $( "#bubble" ).draggable();
+    $( "#cloth" ).draggable();
+    $( "#shells").draggable();
     $( "#beads" ).draggable();
+    $( "#dress" ).draggable();  
+    $( "#hair" ).draggable();
+    $( "#body" ).draggable();
 });
 });
+
+
+/**Game**/
+$("#bubble").click(function(){
+    $("#speech").html("Round 2");
+});
+
+
+/**History**/
+$("#string").click(function(){
+    $("#text").html("text");
+    $(this).css("border", "solid");
+});
+
+$("#cotton").click(function(){
+    $("#text").html("text");
+    $(this).css("border", "solid");
+});
+
+$("#corn").click(function(){
+    $("#text").html("text");
+    $(this).css("border", "solid");
+});
+
+$("#shells").click(function(){
+    $("#text").html("text");
+    $(this).css("border", "solid");
+});
+ 
+$("#beads").click(function(){
+    $("#text").html("text");
+    $(this).css("border", "solid");
+});
+
+
+$("#cloth").click(function(){
+    $("#text").html("text");
+    $(this).css("border", "solid");
+});
+
 
   $(function() {
     $( "#draggable" ).draggable();
@@ -19,8 +64,7 @@ $(function() {
       drop: function( event, ui ) {
         $( this )
           .addClass( "ui-state-highlight" )
-          .find( "p" )
-            .html( "You have selected _______!" );
+            .html("");
       }
     });
   });
